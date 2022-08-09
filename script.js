34e41aa815e8eb4972c7c983a7b2340bb18899de
Guilherme <guilhermeisk@hotmail.com>
2022-08-09 10:56:30 -0300

    Projeto 3

---
+++ script.js
@@ -0,0 +1,17 @@
+function calcular(){
+  let peso = document.getElementById('peso').value
+  let altura = document.getElementById('altura').value
+  let texto= document.getElementById('resultado')
+  let resultado= peso/(altura*altura)
+  const resultadoFinal=resultado.toFixed(1)
+
+
+  if(resultadoFinal<=18.5){
+    texto.innerHTML='Abaixo do peso normal: ' + resultadoFinal
+  } else if(resultadoFinal>=18.5 && resultadoFinal<=24.9){
+    texto.innerHTML='Peso ideal: ' + resultadoFinal
+  } else if(resultadoFinal>=25 && resultadoFinal<=29.9){
+    texto.innerHTML='Excesso de peso: ' + resultadoFinal
+  } else{
+    texto.innerHTML='Obesidade: procure um médico' }
+}
